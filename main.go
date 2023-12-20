68e2d829379a79a20d52b8f8386e09e1b2e98c1b
Saint <p.leelayuthto@hotmail.com>
2023-12-20 21:06:46 +0700

    success get and post method

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Movie struct {
+	ImdbID      string  `json:"imdbID"`
+	Title       string  `json:"title"`
+	Year        int     `json:"year"`
+	Rating      float32 `json:"rating"`
+	IsSuperHero bool    `json:"isSuperHero"`
+}
+
+var movies []Movie
+
+func moviesHandler(res http.ResponseWriter, req *http.Request) {
+	method := req.Method
+
+	if method == "GET" {
+		b, err := json.Marshal(movies)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(res, "error: %s", err)
+			return
+		}
+		res.WriteHeader(http.StatusOK)
+		res.Header().Set("Content-Type", "application/json; charset=utf-8")
+		res.Write(b)
+	} else if method == "POST" {
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(res, "error : %v", err)
+			return
+		}
+		t := Movie{}
+		err = json.Unmarshal(body, &t)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(res, "error: %s", err)
+			return
+		}
+		movies = append(movies, t)
+		res.WriteHeader(http.StatusOK)
+		b, err := json.Marshal(movies)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(res, "error: %s", err)
+			return
+		}
+		res.Write(b)
+		return
+	}
+}
 
 func main() {
-	fmt.Println("Go movie api")
+	http.HandleFunc("/movies", moviesHandler)
+	err := http.ListenAndServe("localhost:8888", nil)
+	log.Fatal(err)
 }
